2dbd8b9961b0e7197bbf13007c958acbaa449305
leafee98 <leafee98@hotmail.com>
2019-09-11 21:07:32 +0800

    writing radix function, researching how to show it on nevigation

--- app/src/main/java/com/example/calculatorbyleafee/MainCalc.java
+++ app/src/main/java/com/example/calculatorbyleafee/MainCalc.java
@@ -1,102 +1,55 @@
 package com.example.calculatorbyleafee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.Menu;
 
-import com.example.calculatorbyleafee.lib.expression.ErrorExpressionException;
-import com.example.calculatorbyleafee.lib.expression.Expression;
-
-import java.util.Arrays;
-import java.util.List;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainCalc extends AppCompatActivity {
 
-    private static class InputHandler implements View.OnClickListener {
-        private TextView textView;
-        private StringBuilder expression = new StringBuilder();
-
-        public InputHandler(TextView textView) {
-            this.textView = textView;
-        }
-
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.button_numPoint: expression.append('.'); break;
-                case R.id.button_num0: expression.append('0'); break;
-                case R.id.button_num1: expression.append('1'); break;
-                case R.id.button_num2: expression.append('2'); break;
-                case R.id.button_num3: expression.append('3'); break;
-                case R.id.button_num4: expression.append('4'); break;
-                case R.id.button_num5: expression.append('5'); break;
-                case R.id.button_num6: expression.append('6'); break;
-                case R.id.button_num7: expression.append('7'); break;
-                case R.id.button_num8: expression.append('8'); break;
-                case R.id.button_num9: expression.append('9'); break;
-                case R.id.button_opPlus: expression.append('+'); break;
-                case R.id.button_opMinus: expression.append('-'); break;
-                case R.id.button_opMulti: expression.append('*'); break;
-                case R.id.button_opDiv: expression.append('/'); break;
-                case R.id.button_opPow: expression.append('^'); break;
-                case R.id.button_opSin: expression.append("sin("); break;
-                case R.id.button_opCos: expression.append("cos("); break;
-                case R.id.button_opTan: expression.append("tan("); break;
-                case R.id.button_opMod: expression.append("mod"); break;
-                case R.id.button_opLeftParentheses: expression.append('('); break;
-                case R.id.button_opRightParentheses: expression.append(')'); break;
-
-                case R.id.button_clearAll:
-                    expression = new StringBuilder();
-                    break;
-
-                case R.id.button_backspace:
-                    try {
-                        expression.deleteCharAt(expression.length() - 1);
-                    } catch (StringIndexOutOfBoundsException e) {
-                        Log.d("input expression",
-                                "backspace while expression zero length");
-                    }
-                    break;
-                case R.id.button_opEqual:
-                    try {
-                        expression = new StringBuilder(
-                                String.format("%t .7f",
-                                        Expression.calculate(expression.toString(), true)));
-                    } catch (ErrorExpressionException e) {
-                        expression = new StringBuilder(e.getMessage());
-                    }
-                    break;
-            }
-
-            textView.setText(expression.toString());
-        }
-    }
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_calc);
+        setContentView(R.layout.activity_navigation_drawer);
 
-        List<Integer> buttonsId = Arrays.asList(
-                R.id.button_num0, R.id.button_num1, R.id.button_num2, R.id.button_num3,
-                R.id.button_num4, R.id.button_num5, R.id.button_num6, R.id.button_num7,
-                R.id.button_num8, R.id.button_num9, R.id.button_numPoint,
-                R.id.button_opPlus, R.id.button_opMinus, R.id.button_opMulti, R.id.button_opDiv,
-                R.id.button_opLeftParentheses, R.id.button_opRightParentheses,
-                R.id.button_opPow, R.id.button_opMod, R.id.button_opEqual,
-                R.id.button_opSin, R.id.button_opCos, R.id.button_opTan,
-                R.id.button_clearAll, R.id.button_backspace
-        );
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
-        InputHandler inputHandler = new InputHandler((TextView)findViewById(R.id.textView_expressionShower));
-        for (int i = 0; i < buttonsId.size(); ++i) {
-            ((Button)findViewById(buttonsId.get(i))).setOnClickListener(inputHandler);
-        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/example/calculatorbyleafee/ui/home/HomeFragment.java
@@ -0,0 +1,113 @@
+package com.example.calculatorbyleafee.ui.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.calculatorbyleafee.MainCalc;
+import com.example.calculatorbyleafee.R;
+import com.example.calculatorbyleafee.lib.expression.ErrorExpressionException;
+import com.example.calculatorbyleafee.lib.expression.Expression;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    private static class InputHandler implements View.OnClickListener {
+        private TextView textView;
+        private StringBuilder expression = new StringBuilder();
+
+        public InputHandler(TextView textView) {
+            this.textView = textView;
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.button_numPoint: expression.append('.'); break;
+                case R.id.button_num0: expression.append('0'); break;
+                case R.id.button_num1: expression.append('1'); break;
+                case R.id.button_num2: expression.append('2'); break;
+                case R.id.button_num3: expression.append('3'); break;
+                case R.id.button_num4: expression.append('4'); break;
+                case R.id.button_num5: expression.append('5'); break;
+                case R.id.button_num6: expression.append('6'); break;
+                case R.id.button_num7: expression.append('7'); break;
+                case R.id.button_num8: expression.append('8'); break;
+                case R.id.button_num9: expression.append('9'); break;
+                case R.id.button_opPlus: expression.append('+'); break;
+                case R.id.button_opMinus: expression.append('-'); break;
+                case R.id.button_opMulti: expression.append('*'); break;
+                case R.id.button_opDiv: expression.append('/'); break;
+                case R.id.button_opPow: expression.append('^'); break;
+                case R.id.button_opSin: expression.append("sin("); break;
+                case R.id.button_opCos: expression.append("cos("); break;
+                case R.id.button_opTan: expression.append("tan("); break;
+                case R.id.button_opMod: expression.append("mod"); break;
+                case R.id.button_opLeftParentheses: expression.append('('); break;
+                case R.id.button_opRightParentheses: expression.append(')'); break;
+
+                case R.id.button_clearAll:
+                    expression = new StringBuilder();
+                    break;
+
+                case R.id.button_backspace:
+                    try {
+                        expression.deleteCharAt(expression.length() - 1);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        Log.d("input expression",
+                                "backspace while expression zero length");
+                    }
+                    break;
+                case R.id.button_opEqual:
+                    if (expression.toString().length() > 0) {
+                        try {
+                            expression = new StringBuilder(
+                                    String.format("%.7f",
+                                            Expression.calculate(expression.toString(), true)));
+                        } catch (ErrorExpressionException e) {
+                            expression = new StringBuilder(e.getMessage());
+                        }
+                    }
+                    break;
+            }
+
+            textView.setText(expression.toString());
+        }
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // set onClick Listener
+        List<Integer> buttonsId = Arrays.asList(
+                R.id.button_num0, R.id.button_num1, R.id.button_num2, R.id.button_num3,
+                R.id.button_num4, R.id.button_num5, R.id.button_num6, R.id.button_num7,
+                R.id.button_num8, R.id.button_num9, R.id.button_numPoint,
+                R.id.button_opPlus, R.id.button_opMinus, R.id.button_opMulti, R.id.button_opDiv,
+                R.id.button_opLeftParentheses, R.id.button_opRightParentheses,
+                R.id.button_opPow, R.id.button_opMod, R.id.button_opEqual,
+                R.id.button_opSin, R.id.button_opCos, R.id.button_opTan,
+                R.id.button_clearAll, R.id.button_backspace
+        );
+
+        InputHandler inputHandler =
+                new InputHandler((TextView)root.findViewById(R.id.textView_expressionShower));
+        for (int i = 0; i < buttonsId.size(); ++i) {
+            (root.findViewById(buttonsId.get(i))).setOnClickListener(inputHandler);
+        }
+
+        return root;
+    }
+}
